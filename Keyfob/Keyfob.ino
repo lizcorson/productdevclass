004cea6367756f639f9cde3b84c16052073fbc0f
Liz Corson <elizabethcorson@gmail.com>
2017-10-27 23:48:45 -0400

    alarm tones set

--- Keyfob/Keyfob.ino
+++ Keyfob/Keyfob.ino
@@ -19,9 +19,13 @@ IRdecode myDecoder;
 #define BUZZPIN 0
 #define LEDPIN 3
  
-int numTones = 10;
-int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+//int numTones = 10;
+//int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
 //            mid C  C#   D    D#   E    F    F#   G    G#   A
+
+int numTones = 2;
+int numLoops = 5;
+int tones[] = {100, 500};
 
 void setup() {
   Serial.begin(9600);
@@ -84,11 +88,13 @@ void blinky() {
 }
 
 void sounds() {
-  for (int i = 0; i < numTones; i++) {
-    tone(SPEAKERPIN, tones[i]);
-    delay(500);
+  for (int j=0; j < numLoops; j++) {
+    for (int i = 0; i < numTones; i++) {
+      tone(SPEAKERPIN, tones[i]);
+      delay(500);
+    }
+    noTone(SPEAKERPIN);
   }
-  noTone(SPEAKERPIN);
 }
 
 void buzz() {
--- piezotest/piezotest.ino
+++ piezotest/piezotest.ino
@@ -1,17 +1,22 @@
 #define SPEAKERPIN 4
-int numTones = 10;
+/*int numTones = 10;
 int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
-//            mid C  C#   D    D#   E    F    F#   G    G#   
+//            mid C  C#   D    D#   E    F    F#   G    G#   */
+
+int numTones = 2;
+int tones[] = {100, 500};
+
 void setup() {
   // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
   for (int i = 0; i < numTones; i++) {
     tone(SPEAKERPIN, tones[i]);
     delay(500);
   }
   noTone(SPEAKERPIN);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
 
 }
